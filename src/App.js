3104cd43bacdbc5148b5d5da5b1dde9e0ac4b2a5
Gaurav Jayasawal <jgaurav6@gmail.com>
2018-10-26 19:33:27 -0400

    moved navbar to different file

--- src/App.js
+++ src/App.js
@@ -1,64 +1,18 @@
 import React, { Component } from "react";
-import SideDrawer from "./components/SideDrawer/SideDrawer";
-import Toolbar from "./components/Toolbar/Toolbar";
 import "./App.css";
 // import Button from "./components/Button/Button";
 import BackgroundDiv from "./components/BgDiv/BackgroundDiv";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import Backdrop from "./components/Backdrop/Backdrop";
-library.add(faBars);
+import Navbar from "./components/Navbar/Navbar";
 
 class App extends Component {
-  state = {
-    SideDrawerOpen: false
-  };
-
-  navItems = [
-    { id: "item1", value: "PROJECTS" },
-    { id: "item2", value: "PORTFOLIO" },
-    { id: "item3", value: "ABOUT" },
-    { id: "item4", value: "SKILLS" }
-  ];
-
-  drawerToggleClickHandler = () => {
-    this.setState(prevState => {
-      return {
-        SideDrawerOpen: !prevState.SideDrawerOpen
-      };
-    });
-  };
-
-  backdropClickHandler = () => {
-    this.setState({ SideDrawerOpen: false });
-  };
-
   render() {
-    let backdrop;
-
-    if (this.state.SideDrawerOpen) {
-      backdrop = <Backdrop click={this.backdropClickHandler} />;
-    }
     return (
       <div className="App">
         <div className="bgDiv">
           <BackgroundDiv className="divLeft" color="#33759B" />
           <BackgroundDiv color="#FFECEC" className="divRight" />
         </div>
-        <Toolbar
-          drawerClickHandle={this.drawerToggleClickHandler}
-          navItems={this.navItems}
-          buttonText="HIRE ME"
-        />
-        {/* <div className="hireMeButton">
-          <Button text="HIRE ME" />
-        </div> */}
-        <SideDrawer
-          show={this.state.SideDrawerOpen}
-          navItems={this.navItems}
-          buttonText="HIRE ME"
-        />
-        {backdrop}
+        <Navbar />
       </div>
     );
   }
